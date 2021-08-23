3a4bbebe91679496794ef4c6934da329f20a985e
Jesse Schmidt <jschmidt@aerospike.com>
2021-08-22 19:54:50 -0700

    Remove feature-key

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,21 @@ if [ -n "$3" ] || [ -n "$4" ]; then
   config_dir="$GITHUB_WORKSPACE/$(dirname $3)"
   echo $config_dir
   ls -la $config_dir
-  mount="-v /var/run/docker.sock:/var/run/docker.sock -v $config_dir:/opt/aerospike/etc"
+  mount="-v /var/run/docker.sock:/var/run/docker.sock -v $config_dir:/etc/aerospike"
 fi
 
-if [ -n "$4" ]; then
-  feature_key_file=$(basename $4)
-  image="-e \"FEATURE_KEY_FILE=/opt/aerospike/etc/$feature_key_file\" aerospike/aerospike-server-enterprise:$2"
-else
-  image="aerospike/aerospike-server:$2"
-fi
+# if [ -n "$4" ]; then
+  # feature_key_file=$(basename $4)
+  # image="-e \"FEATURE_KEY_FILE=/opt/aerospike/etc/$feature_key_file\" aerospike/aerospike-server-enterprise:$2"
+# else
+  # image="aerospike/aerospike-server:$2"
+# fi
+image="aerospike/aerospike-server:$2"
 
 if [ -n "$3" ]; then
   config_file=$(basename $3)
   docker_cmd="docker run -d --privileged --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 \
-  $mount $image --config-file /opt/aerospike/etc/$config_file"
+  $mount $image --config-file /etc/aerospike/$config_file"
 else
   docker_cmd="docker run -d --name gha_aerospike -p $1:3000 -p 3001:3001 -p 3002:3002 -p 3003:3003 $image"
 fi
